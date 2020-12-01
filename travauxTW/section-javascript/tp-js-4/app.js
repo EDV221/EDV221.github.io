5c7533c8aa7580bc94e4b8f0ceec95aab04fa327
EDV221 <73819456+EDV221@users.noreply.github.com>
2020-12-01 09:25:59 +0100

    update V2 TP JS 4

--- travauxTW/section-javascript/tp-js-4/app.js
+++ travauxTW/section-javascript/tp-js-4/app.js
@@ -2,7 +2,7 @@ window.addEventListener('load', () => {
 	registerSW();
 
   let cors_url = 'https://cors-anywhere.herokuapp.com/';
-  let url = 'https://planning.univ-rennes1.fr/jsp/custom/modules/plannings/9EYlGR3a.shu'
+  let url = 'https://planning.univ-rennes1.fr/jsp/custom/modules/plannings/9EYlGR3a.shu';
 
 
   fetch( cors_url + url ).then(function(response){
